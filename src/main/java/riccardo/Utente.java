6d520dbd6f3e1e64e759b1f2e17ce9c56a036f5f
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-04-18 16:49:57 +0200

    aggiunta costruttore, getter e setter

--- src/main/java/riccardo/Utente.java
+++ src/main/java/riccardo/Utente.java
@@ -4,5 +4,34 @@ public class Utente {
     private String nome;
     private String surname;
     private int eta;
-    
+
+    public Utente(String nome, String surname, int eta) {
+        this.nome = nome;
+        this.surname = surname;
+        this.eta = eta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
 }
